c15a87aa5e8ae15f495abe625368a8c6d58f7660
dat nguyen 9 <dat.nguyen9@sotatek.com>
2022-08-11 14:05:49 +0700

    add layout

---
+++ src/components/GlobalStyles/Layout/components/Header/index.js
@@ -0,0 +1,13 @@
+import classNames from "classnames";
+import styles from "./Header.module.scss";
+
+const cx = classNames.bind(styles);
+function Header() {
+  return <header className={cx('wrapper')}>
+      <div className={cx('inner')}>
+          header
+      </div>
+  </header>
+}
+
+export default Header;
